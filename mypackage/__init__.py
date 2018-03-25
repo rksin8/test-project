b48628f395935320c0fc5ccd3ae419d71aa330b7
rksin8 <ranjeetbhu88@gmail.com>
2018-03-26 05:09:24 +0530

    mypackage added

---
+++ mypackage/__init__.py
@@ -0,0 +1,10 @@
+from . import subpackage1
+from . import subpackage2
+
+
+
+
+#__all__=["module1","module2","module3"]
+
+#__all__ =["subpackage1","subpackage1"]
+
---
+++ mypackage/subpackage1/__init__.py
@@ -0,0 +1,4 @@
+from . import moduleX
+from . import moduleY
+
+
